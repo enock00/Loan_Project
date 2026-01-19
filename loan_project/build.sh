c546539fb25503caf92f41eae3690c8c1e724984
Enock <enockzy54@gmail.com>
2026-01-19 12:46:40 +0300

    Fix DJANGO_SETTINGS_MODULE for Render

--- loan_project/build.sh
+++ loan_project/build.sh
@@ -2,5 +2,5 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
+python loan_project/manage.py migrate
+python loan_project/manage.py collectstatic --noinput
